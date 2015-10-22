28db508fa95cae27bebb5bac1d1dcb32d15ce34c
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2015-10-22 14:25:40 -0200

    fixed mencoder; +overleaf

--- mencoder.sh
+++ mencoder.sh
@@ -7,3 +7,8 @@
 
 ls plot.* | sort -n -t . -k 2 > list.txt
 mencoder "mf://@list.txt" -mf fps=$1 -o $2 -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=800
+
+#mencoder dvd://1 -o /dev/null -ss 32 -ovc x264 -x264encopts pass=1:turbo:bitrate=900:bframes=1:me=umh:partitions=all:trellis=1:qp_step=4:qcomp=0.7:direct_pred=auto:keyint=300 -vf crop=720:352:0:62,scale=-10:-1,harddup -oac faac -faacopts br=192:mpeg=4:object=2 -channels 2 -srate 48000 -ofps 24000/1001
+
+#mencoder dvd://1 -o narnia.avi -ss 32 -ovc x264 -x264encopts pass=2:turbo:bitrate=900:frameref=5:bframes=1:me=umh:partitions=all:trellis=1:qp_step=4:qcomp=0.7:direct_pred=auto:keyint=300 -vf crop=720:352:0:62,scale=-10:-1,harddup -oac faac -faacopts br=192:mpeg=4:object=2 -channels 2 -srate 48000 -ofps 24000/1001
+
---
+++ overleaf.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Script that sets up the Overleaf git server and a possible
+# Github server as well for your latex paper. You first need to
+# create an overleaf project for this to work.
+#
+# https://ineed.coffee/3454/how-to-synchronize-an-overleaf-latex-paper-with-a-github-repository/
+
+# Gets URLs of git servers
+read -p "Overleaf git server: " overleaf # click share button in your project
+read -p "Github or BitBucket server: " github # address of git project
+read -p "Destination folder name: " folder 
+
+# Downloads overleaf project 
+git clone $overleaf $folder
+git remote rename origin overleaf
+
+# Adds Github server
+case $github in
+    *git ) git remote add github $github ; break;;
+    * ) echo "No Github/Bitbucket server.";;
+esac
+
+# Be able to push to both overleaf and github (git push both)
+git remote add both $overleaf
+git remote set-url --add --push both $overleaf
+case $github in
+    *git ) git remote set-url --add --push both $github ; break;;
+esac
