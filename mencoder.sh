4fa5a507b57ce3b94428081019be6118ad356075
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2016-08-29 10:37:30 -0300

    improved mencoder.sh based on a script I wrote for PLUTO

--- mencoder.sh
+++ mencoder.sh
@@ -2,13 +2,32 @@
 # 
 # Creates movie file based on sequential images.
 #
-# Usage: mencoder.sh fps output.avi
-# 	e.g. mencoder.sh 5 movie.avi
+# Usage: mencoder.sh <fps> <output file basename>
+# 	e.g. mencoder.sh 5 movie
 
-ls plot.* | sort -n -t . -k 2 > list.txt
-mencoder "mf://@list.txt" -mf fps=$1 -o $2 -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=800
+bitrate=900
 
-#mencoder dvd://1 -o /dev/null -ss 32 -ovc x264 -x264encopts pass=1:turbo:bitrate=900:bframes=1:me=umh:partitions=all:trellis=1:qp_step=4:qcomp=0.7:direct_pred=auto:keyint=300 -vf crop=720:352:0:62,scale=-10:-1,harddup -oac faac -faacopts br=192:mpeg=4:object=2 -channels 2 -srate 48000 -ofps 24000/1001
+# check if there were command-line arguments
+if [ $# -eq 0 ]; then
+    echo "Usage: "
+    echo "  mencoder.sh <fps> <output file basename> "
+    exit 1
+fi
 
-#mencoder dvd://1 -o narnia.avi -ss 32 -ovc x264 -x264encopts pass=2:turbo:bitrate=900:frameref=5:bframes=1:me=umh:partitions=all:trellis=1:qp_step=4:qcomp=0.7:direct_pred=auto:keyint=300 -vf crop=720:352:0:62,scale=-10:-1,harddup -oac faac -faacopts br=192:mpeg=4:object=2 -channels 2 -srate 48000 -ofps 24000/1001
 
+# creates sequential list of files that will be rendered
+ls plot.*.jpeg | sort -n -t . -k 2 > list.txt
+
+mencoder "mf://@list.txt" -mf fps=$1 -o $2.avi -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=$bitrate
+
+# Quicktime compatible movie (OS X)
+# ===========================
+# http://www.mplayerhq.hu/DOCS/HTML/en/menc-feat-quicktime-7.html
+
+# pass 1
+#mencoder "mf://@list.txt" -ovc x264 -x264encopts pass=1:turbo:bitrate=$bitrate:bframes=1:me=umh:partitions=all:trellis=1:qp_step=4:qcomp=0.7:direct_pred=auto:keyint=300:threads=auto -oac faac -faacopts br=192:mpeg=4:object=2 -channels 2 -srate 48000 -mf fps=$1 
+
+# pass 2
+#mencoder "mf://@list.txt" -o movie.avi -ovc x264 -x264encopts pass=2:turbo:bitrate=$bitrate:frameref=5:bframes=1:me=umh:partitions=all:trellis=1:qp_step=4:qcomp=0.7:direct_pred=auto:keyint=300:threads=auto -oac faac -faacopts br=192:mpeg=4:object=2 -channels 2 -srate 48000 -mf fps=$1 
+
+ffmpeg -i $2.avi -acodec libmp3lame -ab 192 movie.mov
